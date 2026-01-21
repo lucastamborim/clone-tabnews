c64c76d09a7b722a701b2f361b68c63bf808d8e7
Lucas Tamborim <lucastamborimdasilva@gmail.com>
2026-01-21 20:25:06 +0000

    Adiciona scripts `lint:check` e `lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return (
-        <div>
-            <h1> Oii Claraaa! </h1>   
-            <button onClick={()=> alert("Te amo") }> Clica aqui </button>
-        </div>
-    )
+  return (
+    <div>
+      <h1> Oii Claraaa! </h1>
+      <button onClick={() => alert("Te amo")}> Clica aqui </button>
+    </div>
+  );
 }
 
 export default Home;
